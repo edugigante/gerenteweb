09e6f00a865e3f3ec201511c259b329f08dbce2a
Eduardo Guimraes <projetos@gerenteweb.com>
2012-02-12 15:50:38 -0200

    teste p/ mathias
    Signed-off-by: Eduardo Guimraes <projetos@gerenteweb.com>

---
+++ index.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+    <title>Teste de Conversão</title>
+</head>
+<body>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-25705334-1']);
+  _gaq.push(['_setDomainName', 'viseo.com.br']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+<a href="conversao.php">Conversao</a>
+</body>
+</html>
